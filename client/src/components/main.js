72de581812d0b0637e79a334dbd81a10a4dbb54f
unknown <dillipkumar.sahu@gmail.com>
2016-08-20 18:56:09 +0530

    last commit

---
+++ client/src/components/main.js
@@ -0,0 +1,7 @@
+
+var React=require('react');
+var ReactDOM=require('react-dom');
+var Weather=require('./Weather');
+var NavBar=require('../NavBar/NavBar');
+
+ReactDOM.render(<Weather/>,document.getElementById('app'));
